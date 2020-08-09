616f15e1de67c89aeb77235de2b07f7f68cff01d
TranHoangViet <viet14042000@gmail.com>
2020-08-09 17:15:22 +0700

    create visualization board and physical board

--- agents.py
+++ agents.py
@@ -1,7 +1,66 @@
-from game import Board
+#from game import Board
 class Player():
     HUMAN = True
     AI = False
+
+
+class Agent:
+    """
+    An agent must define a getAction method, but may also define the
+    following methods which will be called if they exist:
+
+    def registerInitialState(self, state): # inspects the starting state
+    """
+
+    def __init__(self, index=0):
+        self.index = index
+
+    def getAction(self, state):
+        """
+        The Agent will receive a GameState (from either {pacman, capture, sonar}.py) and
+        must return an action from Directions.{North, South, East, West, Stop}
+        """
+        raiseNotDefined()
+
+
+def heuristic(currBoard, playerID):
+    score = 0
+    if (playerID == Player.HUMAN):
+        score += 1
+    else:
+        score -= 1
+    if (currBoard.isWin(playerID)):
+        if (playerID == Player.HUMAN):
+            score += 99_999_999
+        else:
+            score -= 99_999_999
+
+    for i in range(currBoard.countThreeContinousNoBound(playerID)):
+        if (playerID == Player.HUMAN):
+            score += 100
+        else:
+            score -= 100
+
+    for i in range(currBoard.countThreeContinousBound(playerID)):
+        if (playerID == Player.HUMAN):
+            score += 80
+        else:
+            score -= 80
+
+    for i in range(currBoard.countTwoContinousNoBound(playerID)):
+        if (playerID == Player.HUMAN):
+            score += 30
+        else:
+            score -= 30
+
+    for i in range(currBoard.countTwoContinousBound(playerID)):
+        if (playerID == Player.HUMAN):
+            score += 10
+        else:
+            score -= 10
+
+    return score
+
 
 class MultiAgentSearchAgent(Agent):
     """
@@ -18,10 +77,11 @@ class MultiAgentSearchAgent(Agent):
     is another abstract class.
     """
 
-    def __init__(self, evalFn, depth = '5'):
+    def __init__(self, depth='5'):
         self.player = Player.AI
-        self.evaluationFunction = evalFn
+        self.evaluationFunction = heuristic
         self.depth = int(depth)
+
 
 class MinimaxAgent(MultiAgentSearchAgent):
 
@@ -31,7 +91,8 @@ class MinimaxAgent(MultiAgentSearchAgent):
                 return self.evaluationFunction(currBoard)
             val = -99_999_999
             for action in currBoard.getEmptySpace():
-                val = max(val, minValue(currBoard.set(action[0], action[1], player), not player, currDepth))
+                val = max(val, minValue(currBoard.set(
+                    action[0], action[1], player), not player, currDepth))
             return val
 
         def minValue(currBoard, player, currDepth):
@@ -39,21 +100,25 @@ class MinimaxAgent(MultiAgentSearchAgent):
                 return self.evaluationFunction(currBoard)
             val = 99_999_999
             for action in currBoard.getEmptySpace():
-                val = min(val, maxValue(currBoard.set(action[0], action[1], player), not player, currDepth + 1))
+                val = min(val, maxValue(currBoard.set(
+                    action[0], action[1], player), not player, currDepth + 1))
             return val
 
         # Initial call:
         act = board.getEmptySpace()[0]
-        maxVal = minValue(board.set(act[0], act[1], self.player), self.evaluationFunction, not self.player, 0)
+        maxVal = minValue(board.set(
+            act[0], act[1], self.player), not self.player, 0)
         for action in board.getEmptySpace(0):
             if action == board.getEmptySpace(0)[0]:
                 continue
-            currVal = minValue(board.set(action[0], action[1], self.player), self.evaluationFunction, not self.player, 0)
+            currVal = minValue(board.set(
+                action[0], action[1], self.player), not self.player, 0)
             if currVal > maxVal:
                 maxVal = currVal
                 act = action
 
         return act
+
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
     def getLocation(self, board):
@@ -66,8 +131,9 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
                 return self.evaluationFunction(currBoard)
             val = -99_999_999
             for action in currBoard.getEmptySpace():
-                val = max(val, minValue(currBoard.set(action[0], action[1], player), not player, currDepth, alpha, beta))
-               
+                val = max(val, minValue(currBoard.set(
+                    action[0], action[1], player), not player, currDepth, alpha, beta))
+
                 if val > beta:
                     return val
                 alpha = max(val, alpha)
@@ -78,8 +144,9 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
                 return self.evaluationFunction(currBoard)
             val = 99_999_999
             for action in currBoard.getEmptySpace():
-                val = min(val, maxValue(currBoard.set(action[0], action[1], player), not player, currDepth + 1, alpha, beta))    
-            
+                val = min(val, maxValue(currBoard.set(
+                    action[0], action[1], player), not player, currDepth + 1, alpha, beta))
+
                 if val < alpha:
                     return val
                 beta = min(val, beta)
@@ -90,50 +157,12 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         beta = 99_999_999
         act = board.getEmptySpace()[0]
         for action in board.getEmptySpace():
-            currVal = minValue(board.currBoard.set(action[0], action[1], self.player), not self.player, 0, alpha, beta)
+            currVal = minValue(board.currBoard.set(
+                action[0], action[1], self.player), not self.player, 0, alpha, beta)
             if currVal > val:
                 act = action
                 val = currVal
-                
+
                 alpha = max(val, alpha)
-        
+
         return act
-
-def heuristic(currBoard,playerID):
-    score = 0
-    if (playerID==Player.HUMAN):
-        score+=1
-    else:
-        score-=1
-    if (currBoard.isWin(playerID)):
-        if (playerID==Player.HUMAN):
-            score+=99_999_999
-        else:
-            score-=99_999_999
-    
-    for i in range(currBoard.countThreeContinousNoBound(playerID)):
-        if (playerID==Player.HUMAN):
-            score+=100
-        else:
-            score-=100
-
-    for i in range(currBoard.countThreeContinousBound(playerID)):
-        if (playerID==Player.HUMAN):
-            score+=80
-        else: 
-            score-=80
-    
-    for i in range(currBoard.countTwoContinousNoBound(playerID)):
-        if (playerID==Player.HUMAN):
-            score+=30
-        else:
-            score-=30
-    
-    for i in range(currBoard.countTwoContinousBound(playerID)):
-        if (playerID==Player.HUMAN):
-            score+=10
-        else:
-            score-=10
-    
-
-    return score
--- game.py
+++ game.py
@@ -3,10 +3,16 @@ from functools import partial
 import threading
 import socket
 from tkinter import messagebox
-
+import argparse
+import sys
 from enum import Enum
 import copy
+import tkinter.font as tkFont
 from PIL import Image, ImageTk
+
+from agents import MultiAgentSearchAgent
+from agents import MinimaxAgent
+from agents import AlphaBetaAgent
 
 
 class Player():
@@ -14,21 +20,23 @@ class Player():
     AI = False
 
 
-class Welcome(tk.Tk):
-    def __init__(self):
+class StartScreen(tk.Tk):
+    def __init__(self, agentType):
         super().__init__()
         self.title("Fight with AI Caro CSE HCMUT")
-        self.geometry('500x500')
+        self.geometry('700x500')
+        self.agent = agentType
 
     def buildFrame(self):
         title = tk.Frame(self)
         title.pack()
+        fontStyle = tkFont.Font(size=15)
         vnu = tk.Label(
-            title, text="VIETNAM NATIONAL UNIVERSITY - HO CHI MINH CITY", font=("32")).grid(row=0)
+            title, text="VIETNAM NATIONAL UNIVERSITY - HO CHI MINH CITY", font=tkFont.Font(size=14), foreground="blue").grid(row=0)
         hmcut = tk.Label(
-            title, text="HO CHI MINH CITY UNIVERSITY OF TECHNOLOGY", font=("32")).grid()
+            title, text="HO CHI MINH CITY UNIVERSITY OF TECHNOLOGY", font=tkFont.Font(size=14), foreground="blue").grid()
         cse = tk.Label(
-            title, text="COMPUTER SCIENCE AND ENGINEERING FACULTY", font=("30")).grid()
+            title, text="COMPUTER SCIENCE AND ENGINEERING FACULTY", foreground="blue").grid()
         imgFrame = tk.Frame(self)
         imgFrame.pack()
         image = Image.open("hcmut.png")
@@ -40,57 +48,69 @@ class Welcome(tk.Tk):
         body.pack()
         subject = tk.Label(body, text="CO306A Introduction to AI").grid()
         _ = tk.Label(
-            body, text="________________________________________________").grid()
+            body, text="____________________________________________________________").grid()
         titleName = tk.Label(
-            body, text="Implement Caro Game", font=("28")).grid()
-        # a = tk.Label(body,
-        #             text="Choose what kind of play do you prefer:").grid()
-        # onePlayerButton = tk.Button(body, text="1 player", width=10, command=partial(
-        #    self.onePlayer))
-        #onePlayerButton.grid(column=0, row=2)
-        # twoPlayerButton = tk.Button(body, text="2 player", width=10, command=partial(
-        #    self.twoPlayer))
-        #twoPlayerButton.grid(column=1, row=2)
+            body, text="Implement Caro Game", font=tkFont.Font(size=20), foreground="red").grid()
+        __ = tk.Label(
+            body, text="____________________________________________________________").grid()
+        a = tk.Label(body,
+                     text="Choose what kind of play do you prefer:").grid(pady=20)
+        optionFrame = tk.Frame(self)
+        optionFrame.pack()
+        onePlayerButton = tk.Button(optionFrame, text="1 player", width=10, command=partial(
+            self.onePlayer))
+        onePlayerButton.grid(row=11, column=0, padx=30)
+        twoPlayerButton = tk.Button(optionFrame, text="2 player", width=10, command=partial(
+            self.twoPlayer))
+        twoPlayerButton.grid(row=11, column=1, padx=30)
 
     def onePlayer(self):
         self.destroy()
-        board = Board()
+        board = BoardVisualizaion(self.agent, 1)
         board.buildFrame()
         board.frameControl.pack_forget()
         a = tk.Label(board.frameLabel,
-                     text="Player1 (YOU): X, Player2 (AI): O. You play first!").grid()
+                     text="Player 1 (YOU): X, Player 0 (AI): O. You play first!").grid()
         board.mainloop()
 
     def twoPlayer(self):
         self.destroy()
-        board = Board()
+        board = BoardVisualizaion(self.agent, 2)
         board.buildFrame()
         a = tk.Label(board.frameLabel,
-                     text="Player1: X, Player2: O. First player: Player1").grid()
+                     text="Player 1: X, Player 0: O. First player: Player1").grid()
         board.mainloop()
 
 
-class Board(tk.Tk):
-    def __init__(self, width=6, height=6):
+class BoardVisualizaion(tk.Tk):
+    def __init__(self, agent, gameType, width=6, height=6):
         super().__init__()
+        print(agent)
+        if agent == "AlphabetAgent":
+            self.agent = AlphaBetaAgent()
+        else:
+            self.agent = MinimaxAgent()
+        self.gameType = gameType
         self.width = width
         self.height = height
         self.title("Fight with AI Caro CSE HCMUT")
         self.geometry('500x350')
-        self.board = [[0 for i in range(width)] for j in range(height)]
-        self.button = [[0 for i in range(width)] for j in range(height)]
+        self.board = Board()
+        self.button = [[2 for i in range(width)] for j in range(height)]
         self.currentTurn = 1
+        self.prevTurn = 0
+        self.validCurrentMove = True
 
     def buildFrame(self):
-        self.frameLabel = tk.Frame(self)
-        self.frameLabel.pack()
-
+        frameLabel = tk.Frame(self)
+        frameLabel.pack()
         frameControl = tk.Frame(self)
         frameControl.pack()
         frameBoard = tk.Frame(self)
         frameBoard.pack()
         self.frameBoard = frameBoard
         self.frameControl = frameControl
+        self.frameLabel = frameLabel
 
         switchButton = tk.Button(frameControl, text="Switch turn", width=10, command=partial(
             self.switchTurn))
@@ -98,34 +118,79 @@ class Board(tk.Tk):
         for x in range(self.height):
             for y in range(self.width):
                 self.button[x][y] = tk.Button(frameBoard, font=('arial', 15, 'bold'), height=1, width=2,
-                                              borderwidth=2, command=partial(self.set, x=x, y=y, playerId=self.currentTurn))
+                                              borderwidth=2, command=partial(self.play, x=x, y=y))
                 self.button[x][y].grid(row=x, column=y)
 
                 self.button[x][y]['text'] = ""
 
     def switchTurn(self):
+        if self.validCurrentMove:
+            self.currentTurn ^= 1
 
-        for x in range(self.height):
-            for y in range(self.width):
-                if self.button[x][y]['text'] == "":
-                    self.button[x][y] = tk.Button(self.frameBoard, font=('arial', 15, 'bold'), height=1, width=2,
-                                                  borderwidth=2, command=partial(self.set, x=x, y=y, playerId=self.currentTurn))
-                    self.button[x][y].grid(row=x, column=y)
+    def play(self, x, y):
+        if self.prevTurn == self.currentTurn:
+            curTurn = "Not your turn. Current turn: hahaa"
+            if self.currentTurn == 1:
+                curTurn += "Player 1"
+            else:
+                curTurn += "Player 0"
+            self.noti("", curTurn)
+            return
+        returnVal = self.board.makeMove(x, y, self.currentTurn)
+        if returnVal >= 0:
+            self.handleButton(x, y, self.currentTurn)
+            if returnVal == 1:
+                self.noti("Winner is", "Player 1")
+                self.newGame()
+            elif returnVal == 0:
+                self.noti("Winner is", "Player 0")
+                self.newGame()
+            self.validCurrentMove = True
+            self.prevTurn = self.currentTurn
+        else:
+            self.noti("", "not empty cell")
+            self.validCurrentMove = False
+        if self.gameType == 1:
+            print("Ai turn")
+            act = self.agent.getLocation(self.board)
 
-                    self.button[x][y]['text'] = ""
+    def handleButton(self, x, y, playerId):
+        if playerId == 1:
+            self.button[x][y]['text'] = 'x'
+            self.button[x][y]["foreground"] = 'red'
+        else:
+            self.button[x][y]['text'] = 'o'
+            self.button[x][y]["foreground"] = 'blue'
+
+    def noti(self, title, msg):
+        messagebox.showinfo(str(title), str(msg))
 
     def newGame(self):
         for x in range(self.height):
             for y in range(self.width):
                 self.button[x][y]['text'] = ""
-                self.board[x][y] = 0
+        self.board.currentTurn = 1
+
+
+class Board():
+    def __init__(self,  width=6, height=6):
+        # super().__init__()
+        self.width = width
+        self.height = height
+        # self.title("Fight with AI Caro CSE HCMUT")
+        # self.geometry('500x350')
+        self.board = [[2 for i in range(width)] for j in range(height)]
+        # self.button = [[2 for i in range(width)] for j in range(height)]
         self.currentTurn = 1
+
+    def noti(self, title, msg):
+        messagebox.showinfo(str(title), str(msg))
 
     def getEmptySpace(self):
         emp = []
         for i in range(self.width):
             for j in range(self.height):
-                if self.board[i][j] == 0:
+                if self.board[i][j] == 2:
                     emp.append((i, j))
         return tuple(emp)
 
@@ -135,51 +200,66 @@ class Board(tk.Tk):
             return -1
         return self.board[x][y]
 
-    def noti(self, title, msg):
-        messagebox.showinfo(str(title), str(msg))
+    def newGame(self):
+        for x in range(self.height):
+            for y in range(self.width):
+                self.board[x][y] = 2
+        self.currentTurn = 1
 
     def set(self, x, y, playerId):
+        """
         if self.currentTurn != playerId:
             # not playerId turn
             curTurn = "Not your turn. Current turn: "
             if self.currentTurn == 1:
                 curTurn += "Player 1"
             else:
-                curTurn += "Player 2"
+                curTurn += "Player 0000"
             self.noti("", curTurn)
             print("not your turn")
+            print(self.currentTurn)
             return -1
-        if self.board[x][y] != 0:
+        """
+        if self.board[x][y] != 2:
             # cell not empty
             print("self not empty")
             return -1
+        curBoard = copy.deepcopy(self)
+        curBoard.board[x][y] = self.currentTurn
+        return curBoard
 
+    def makeMove(self, x, y, playerId):
+        """
+        if self.currentTurn != playerId:
+            # not playerId turn
+            curTurn = "Not your turn. Current turn: "
+            if self.currentTurn == 1:
+                curTurn += "Player 1"
+            else:
+                curTurn += "Player 0"
+            self.noti("", curTurn)
+            print("not your turn")
+            return -1
+        """
+        if self.board[x][y] != 2:
+            # cell not empty
+            print("self not empty")
+            return -1
         self.board[x][y] = self.currentTurn
-        if self.currentTurn == 1:
-            self.button[x][y]['text'] = 'x'
-            self.button[x][y]["foreground"] = 'red'
-        else:
-            self.button[x][y]['text'] = 'o'
-            self.button[x][y]["foreground"] = 'blue'
-
-        self.currentTurn = -self.currentTurn+3
+        self.currentTurn ^= 1
+        print(self.currentTurn)
 
         if self.isWin(playerId):
             if playerId == 1:
-                self.noti("Winner is", "Player 1")
-                self.newGame()
+                return 1
             else:
-                self.noti("Winner is", "Player 2")
-
-        return copy.deepcopy(self.board)
-
-    def undo(self, playerId):
-        pass
+                return 0
+        return 2
 
     def isFull(self, x, y):
         for i in range(self.width):
             for j in range(self.height):
-                if self.board[i][j] == 0:
+                if self.board[i][j] == 2:
                     return False
         return True
 
@@ -256,7 +336,7 @@ class Board(tk.Tk):
         return False
 
     def isLose(self, playerId):
-        return isWin(self, not playerId)
+        return self.isWin(playerId ^ 1)
 
     def isThreeContinous(self, x, y, playerId, opt):
         # opt 1: no bound
@@ -270,7 +350,7 @@ class Board(tk.Tk):
         ind = 0
         while x + ind < self.width:
             if self.board[x+ind][y] != playerId:
-                if self.board[x+ind][y] == -playerId + 3:
+                if self.board[x+ind][y] == playerId ^ 1:
                     cntBound += 1
                 break
             cnt += 1
@@ -280,7 +360,7 @@ class Board(tk.Tk):
         ind = 1
         while x-ind >= 0:
             if self.board[x-ind][y] != playerId:
-                if self.board[x-ind][y] == -playerId + 3:
+                if self.board[x-ind][y] == playerId ^ 1:
                     cntBound += 1
                 break
             cnt += 1
@@ -298,7 +378,7 @@ class Board(tk.Tk):
         ind = 0
         while y + ind < self.height:
             if self.board[x][y+ind] != playerId:
-                if self.board[x][y+ind] == -playerId + 3:
+                if self.board[x][y+ind] == playerId ^ 1:
                     cntBound += 1
                 break
             cnt += 1
@@ -308,7 +388,7 @@ class Board(tk.Tk):
         ind = 1
         while y-ind >= 0:
             if self.board[x][y-ind] != playerId:
-                if self.board[x][y-ind] == -playerId + 3:
+                if self.board[x][y-ind] == playerId ^ 1:
                     cntBound += 1
                 break
             cnt += 1
@@ -326,7 +406,7 @@ class Board(tk.Tk):
         ind = 0
         while x + ind < self.width and y+ind < self.height:
             if self.board[x+ind][y+ind] != playerId:
-                if self.board[x+ind][y+ind] == -playerId + 3:
+                if self.board[x+ind][y+ind] == playerId ^ 1:
                     cntBound += 1
                 break
             cnt += 1
@@ -336,7 +416,7 @@ class Board(tk.Tk):
         ind = 1
         while x-ind >= 0 and y - ind >= 0:
             if self.board[x-ind][y-ind] != playerId:
-                if self.board[x-ind][y-ind] == -playerId + 3:
+                if self.board[x-ind][y-ind] == playerId ^ 1:
                     cntBound += 1
                 break
             cnt += 1
@@ -355,7 +435,7 @@ class Board(tk.Tk):
         ind = 0
         while x + ind < self.width and y-ind >= 0:
             if self.board[x+ind][y-ind] != playerId:
-                if self.board[x+ind][y-ind] == -playerId + 3:
+                if self.board[x+ind][y-ind] == playerId ^ 1:
                     cntBound += 1
                 break
             cnt += 1
@@ -365,7 +445,7 @@ class Board(tk.Tk):
         ind = 1
         while x-ind >= 0 and y + ind < self.width:
             if self.board[x-ind][y+ind] != playerId:
-                if self.board[x-ind][y+ind] == -playerId + 3:
+                if self.board[x-ind][y+ind] == playerId ^ 1:
                     cntBound += 1
                 break
             cnt += 1
@@ -391,7 +471,7 @@ class Board(tk.Tk):
         ind = 0
         while x + ind < self.width:
             if self.board[x+ind][y] != playerId:
-                if self.board[x+ind][y] == -playerId + 3:
+                if self.board[x+ind][y] == playerId ^ 1:
                     cntBound += 1
                 break
             cnt += 1
@@ -401,7 +481,7 @@ class Board(tk.Tk):
         ind = 1
         while x-ind >= 0:
             if self.board[x-ind][y] != playerId:
-                if self.board[x-ind][y] == -playerId + 3:
+                if self.board[x-ind][y] == playerId ^ 1:
                     cntBound += 1
                 break
             cnt += 1
@@ -419,7 +499,7 @@ class Board(tk.Tk):
         ind = 0
         while y + ind < self.height:
             if self.board[x][y+ind] != playerId:
-                if self.board[x][y+ind] == -playerId + 3:
+                if self.board[x][y+ind] == playerId ^ 1:
                     cntBound += 1
                 break
             cnt += 1
@@ -429,7 +509,7 @@ class Board(tk.Tk):
         ind = 1
         while y-ind >= 0:
             if self.board[x][y-ind] != playerId:
-                if self.board[x][y-ind] == -playerId + 3:
+                if self.board[x][y-ind] == playerId ^ 1:
                     cntBound += 1
                 break
             cnt += 1
@@ -447,7 +527,7 @@ class Board(tk.Tk):
         ind = 0
         while x + ind < self.width and y+ind < self.height:
             if self.board[x+ind][y+ind] != playerId:
-                if self.board[x+ind][y+ind] == -playerId + 3:
+                if self.board[x+ind][y+ind] == playerId ^ 1:
                     cntBound += 1
                 break
             cnt += 1
@@ -457,7 +537,7 @@ class Board(tk.Tk):
         ind = 1
         while x-ind >= 0 and y - ind >= 0:
             if self.board[x-ind][y-ind] != playerId:
-                if self.board[x-ind][y-ind] == -playerId + 3:
+                if self.board[x-ind][y-ind] == playerId ^ 1:
                     cntBound += 1
                 break
             cnt += 1
@@ -475,7 +555,7 @@ class Board(tk.Tk):
         ind = 0
         while x + ind < self.width and y-ind >= 0:
             if self.board[x+ind][y-ind] != playerId:
-                if self.board[x+ind][y-ind] == -playerId + 3:
+                if self.board[x+ind][y-ind] == playerId ^ 1:
                     cntBound += 1
                 break
             cnt += 1
@@ -485,7 +565,7 @@ class Board(tk.Tk):
         ind = 1
         while x-ind >= 0 and y + ind < self.width:
             if self.board[x-ind][y+ind] != playerId:
-                if self.board[x-ind][y+ind] == -playerId + 3:
+                if self.board[x-ind][y+ind] == playerId ^ 1:
                     cntBound += 1
                 break
             cnt += 1
@@ -532,7 +612,31 @@ class Board(tk.Tk):
         return cnt/2
 
 
+class Game:
+    def __init__(self, agentType):
+        self.curGame = StartScreen(agentType)
+
+    def run(self):
+        self.curGame.buildFrame()
+        self.curGame.mainloop()
+
+
+def readCommand():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--agents',
+                        required=True,
+                        help='Agents type: MinimaxAgent, AlphabetaAgent.')
+    return parser
+
+
 if __name__ == "__main__":
-    board = Welcome()
-    board.buildFrame()
-    board.mainloop()
+    try:
+        options = readCommand().parse_args()
+    except:
+        readCommand().print_help()
+        sys.exit(0)
+    if options.agents == 'MinimaxAgent' or options.agents == "AlphabetaAgent":
+        game = Game(options.agents)
+        game.run()
+    else:
+        print("invalid agent")
